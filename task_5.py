637b4036686a40bc5cbee0fc821fd71a1c5a9d0a
AGostyuzhova <36264893+AGostyuzhova@users.noreply.github.com>
2018-02-13 22:53:51 +0300

    Update task_5.py
    Users statistics for the first two tasks

--- task_5.py
+++ task_5.py
@@ -1,1 +1,20 @@
 # Yandex_test
+with open('shkib1.csv') as inf:
+	user_list = []
+	for line in inf:
+		repeat = False
+		user_line = line.split(',')
+		if not user_list:
+			user_stat = {'User_ip':user_line[2],'Count':1,'Bytes_Sum':int(user_line[7])}
+			user_list.append(user_stat)
+		else:
+			for user in user_list:		
+				if (user_line[2] == user['User_ip']):
+					user['Count'] += 1
+					user['Bytes_Sum'] += int(user_line[7])
+					repeat = True
+			if repeat == False:
+				user_stat = {'User_ip':user_line[2],'Count':1,'Bytes_Sum':int(user_line[7])}
+				user_list.append(user_stat)
+for user in user_list: print (user)
+print (len(user_list))
